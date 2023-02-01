351f18863824f0a33388b2e3e18e674d820c7e26
Tim Thompson <58613685+V01D-NULL@users.noreply.github.com>
2023-01-31 22:06:48 -0600

    Update formatting

--- arrays and hashing/medium/top-k-frequent-elements.js
+++ arrays and hashing/medium/top-k-frequent-elements.js
@@ -19,22 +19,22 @@ var topKFrequent = function(nums, k) {
 const sortedHashmap = (nums, k) => {
   if (nums.length === 1) return [nums[0]];
     
-    const map = new Map();
+  const map = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        const entry = map.get(nums[i]);
-        if (entry !== undefined) {
-            entry.push(nums[i]);
-        }
-        else {
-            map.set(nums[i], [nums[i]]);
-        }
-    }
+  for (let i = 0; i < nums.length; i++) {
+    const entry = map.get(nums[i]);
+      if (entry !== undefined) {
+        entry.push(nums[i]);
+      }
+      else {
+        map.set(nums[i], [nums[i]]);
+      }
+  }
 
-    // For those not familiar with javascript, this code does the following:
-    //
-    // Iterate over the hashmap ([...map.values()]), sort the sub arrays from largest to smallest, select 0-k sub arrays, flatten the array,
-    // filter duplicates by converting it to a set, convert the set back to an array and return the result.
-    //
-    return Array.from(new Set([...map.values()].sort((a,b) => b.length - a.length).splice(0, k).flat()));
+  // For those not familiar with javascript, this code does the following:
+  //
+  // Iterate over the hashmap ([...map.values()]), sort the sub arrays from largest to smallest, select 0-k sub arrays, flatten the array,
+  // filter duplicates by converting it to a set, convert the set back to an array and return the result.
+  //
+  return Array.from(new Set([...map.values()].sort((a,b) => b.length - a.length).splice(0, k).flat()));
 }
