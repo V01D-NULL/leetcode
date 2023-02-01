4f446b56219991e13c6f749b0d157e4923cbaa8a
Tim Thompson <58613685+V01D-NULL@users.noreply.github.com>
2023-01-31 22:02:08 -0600

    Top k frequent elements

---
+++ arrays and hashing/medium/top-k-frequent-elements.js
@@ -0,0 +1,40 @@
+//
+// https://leetcode.com/problems/top-k-frequent-elements
+//
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  return sortedHashmap(nums, k);
+};
+
+// Submission results:
+//
+// Memory: Beats 60.41% : 45.1 MB 
+// Runtime: Beats 85.19% : 78 ms
+//
+const sortedHashmap = (nums, k) => {
+  if (nums.length === 1) return [nums[0]];
+    
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const entry = map.get(nums[i]);
+        if (entry !== undefined) {
+            entry.push(nums[i]);
+        }
+        else {
+            map.set(nums[i], [nums[i]]);
+        }
+    }
+
+    // For those not familiar with javascript, this code does the following:
+    //
+    // Iterate over the hashmap ([...map.values()]), sort the sub arrays from largest to smallest, select 0-k sub arrays, flatten the array,
+    // filter duplicates by converting it to a set, convert the set back to an array and return the result.
+    //
+    return Array.from(new Set([...map.values()].sort((a,b) => b.length - a.length).splice(0, k).flat()));
+}
